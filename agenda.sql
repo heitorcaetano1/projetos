51543e56aa11c51a0068cb51213fc27092352533
heitorcaetano1 <102965190+heitorcaetano1@users.noreply.github.com>
2022-04-05 03:13:48 -0300

    Add files via upload
    sql projeto

---
+++ agenda.sql
@@ -0,0 +1,6 @@
+insert into investimentos.açoes (ativo, preço_compra, quantidade, setor, valor_patrimonio) values ('Itausa', '9,87', '2','banco','10,53');
+insert into investimentos.açoes (ativo, preço_compra, quantidade, setor, valor_patrimonio) values ('Copel', '6,20', '3','eletrica','8,13');
+insert into investimentos.açoes (ativo, preço_compra, quantidade, setor, valor_patrimonio) values ('Sanepar', '3,87', '7','saneamento','5,92');
+insert into investimentos.açoes (ativo, preço_compra, quantidade, setor, valor_patrimonio) values ('Santos Brasil', '6,79', '1','porto','5,79');
+insert into investimentos.açoes (ativo, preço_compra, quantidade, setor, valor_patrimonio) values ('Banestes', '5,02', '4','banco','5,05');
+insert into investimentos.açoes (ativo, preço_compra, quantidade, setor, valor_patrimonio) values ('mxrf11', '9,31', '3','fii','10,00');
